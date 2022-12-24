9ade03fd2a39b07ab573e4966fcbb835373c400c
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-12-24 02:23:38 +0100

    changement icone + teste

--- components/auth/Register.tsx
+++ components/auth/Register.tsx
@@ -1,10 +1,43 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
+import TestService from '../../services/TestService';
 
 export default function Register() {
+
+  const testService = new TestService;
+
+  const [Loading, setLoading] = useState(false)
+
+  function test() {
+    setLoading(true);
+    testService.test()
+    .then(
+      data => {
+        alert(data);
+        console.log(data);
+        setLoading(false)
+      }
+    )
+    .catch(
+      err => {
+        alert("ça marche pas");
+        console.log(err.response);
+      }
+    )
+  }
+  
   return (
     <View style={styles.container}>
       <Text>WIP</Text>
+      {Loading ? (
+        <Text>Chargement</Text>
+      ) : (
+        <Text></Text>
+      )}
+      <Button
+        onPress={test}
+        title='Test'
+      />
     </View>
   )
 }
